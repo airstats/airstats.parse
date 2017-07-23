23cdcdc212ab984683ff99e91fcc45c4ffe3e482
Daniel Hawton <daniel@hawton.com>
2017-07-23 01:03:50 -0800

    Initial commit

---
+++ global.php
@@ -0,0 +1,180 @@
+<?php
+
+date_default_timezone_set("UTC");
+
+define('callsign', 0);
+define('cid', 1);
+define('realname', 2);
+define('clienttype', 3);
+define('frequency', 4);
+define('latitude', 5);
+define('longitude', 6);
+define('altitude', 7);
+define('groundspeed', 8);
+define('planned_aircraft', 9);
+define('planned_tascruise', 10);
+define('planned_depairport', 11);
+define('planned_altitude', 12);
+define('planned_destairport', 13);
+define('server', 14);
+define('protrevision', 15);
+define('rating', 16);
+define('transponder', 17);
+define('facilitytype', 18);
+define('visualrange', 19);
+define('planned_revision', 20);
+define('planned_flighttype', 21);
+define('planned_deptime', 22);
+define('planned_actdeptime', 23);
+define('planned_hrsenroute', 24);
+define('planned_minenroute', 25);
+define('planned_hrsfuel', 26);
+define('planned_minfuel', 27);
+define('planned_altairport', 28);
+define('planned_remarks', 29);
+define('planned_route', 30);
+define('planned_depairport_lat', 31);
+define('planned_depairport_lon', 32);
+define('planned_destairport_lat', 33);
+define('planned_destairport_lon', 34);
+define('atis_message', 35);
+define('time_last_atis_received', 36);
+define('time_logon', 37);
+define('heading', 38);
+define('QNH_iHg', 39);
+define('QNH_Mb', 40);
+
+function config($key, $default = null)
+{
+  global $_config;
+  if ($_config[$key]) {
+    return $_config[$key];
+  }
+  else {
+    return $default;
+  }
+}
+
+$dsn = "mysql:host=" . config("db_host") . ";dbname=" . config("db_name") . ";charset=utf8";
+$opt = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES => false];
+$pdo = new PDO($dsn, config("db_user") , config("db_pass") , $opt);
+$readpdo = new PDO($dsn, config("db_user") , config("db_pass") , $opt);
+$prepareds['select_flight'] = "SELECT * FROM `flights` WHERE `callsign`=:callsign AND `vatsim_id`=:vatsim_id AND `created_at` >= DATE_SUB(NOW(), INTERVAL 24 HOUR) ORDER BY created_at DESC LIMIT 1";
+$prepareds['update_flight'] = "UPDATE `flights` SET `aircraft_type`=:aircraft_type, `departure`=:departure, `arrival`=:arrival, `planned_alt`=:planned_alt, `route`=:route, `remarks`=:remarks, `status`=:status, `departed_at`=:departed_at, `arrived_at`=:arrived_at, `arrival_est`=:arrival_est, `lat`=:lat`, `lon`=:lon, `alt`=:alt, `hdg`=:hdg, `spd`=:spd, `missing_count`=:missing_count, `last_update`=:last_update, updated_at=NOW() WHERE `id`=:id";
+$prepareds['insert_flight'] = "INSERT INTO `flights`(`callsign`,`vatsim_id`,`aircraft_type`,`departure`,`arrival`,`planned_alt`,`route`,`remarks`,`status`,`lat`,`lon`,`alt`,`hdg`,`spd`,`last_update`,`created_at`,`updated_at`) VALUES(:callsign, :vatsim_id, :aircraft_type, :departure, :arrival, :planned_alt, :route, :remarks, :status, :lat, :lon, :alt, :hdg, :spd, :last_update, NOW(), NOW())";
+$prepareds['update_missing_count'] = "UPDATE `flights` SET `missing_count`=:missing_count WHERE `id`=:id";
+$prepareds['select_airport'] = "SELECT lat, lon, elevation FROM airports WHERE id=:id";
+$prepareds['select_position'] = "SELECT created_at FROM positions WHERE flight_id=:flight_id ORDER BY created_at DESC LIMIT 1";
+$prepareds['insert_position'] = "INSERT INTO `positions`(`id`,`flight_id`,`lat`,`lon`,`alt`,`spd`,`hdg`,`created_at`,`updated_at`) VALUES(:id, :flight_id, :lat, :lon, :alt, :spd, :hdg, NOW(), NOW())";
+$prepareds['select_missing'] = "SELECT * FROM flights WHERE status NOT LIKE 'Arrived' AND 'status' NOT LIKE 'Incomplete' AND last_update < :current_update";
+$prepareds['update_status'] = "UPDATE flights SET status=:status WHERE id=:id"
+$prepareds['delete_flight'] = "DELETE FROM flights WHERE id=:id";
+$prepareds['delete_positions'] = "DELETE FROM positions WHERE flight_id=:flight_id";
+
+$airport = [];
+
+function checkArrival($lat, $lon, $spd, $arrival)
+{
+  global $readpdo, $prepareds, $airport;
+
+  if ($arrival == "ZZZZ" || $arrival == '' || $arrival == null) {
+    return false;
+  }
+  if (!isset($airport[$arrival])) {
+    $stmt = $readpdo->prepare($prepareds['select_airport'])->execute([":id" => $arrival]);
+    $row = $stmt->fetch();
+    if ($row) {
+      $airport[$arrival]['lat'] = $row['lat'];
+      $airport[$arrival]['lon'] = $row['lon'];
+      $airport[$arrival]['elevation'] = $row['elevation'];
+    } else {
+      return false;
+    }
+  }
+  if (!isset($airport[$arrival])) return false;
+  if (calc_distance($lat, $lon, $airport[$arrival]['lat'], $airport[$arrival]['lon']) < 3 && $spd < 40) {
+    return true;
+  }
+
+  return false;
+}
+
+function checkDeparture($lat, $lon, $spd, $departure))
+{
+  global $readpdo, $prepareds, $airport;
+
+  if ($departure == "ZZZZ" || $departure == '' || $departure == null) {
+    return false;
+  }
+  if (!isset($airport[$departure])) {
+    $stmt = $readpdo->prepare($prepareds['select_airport'])->execute([":id" => $departure]);
+    $row = $stmt->fetch();
+    if ($row) {
+      $airport[$departure]['lat'] = $row['lat'];
+      $airport[$departure]['lon'] = $row['lon'];
+      $airport[$departure]['elevation'] = $row['elevation'];
+    } else {
+      return false;
+    }
+  }
+  if (!isset($airport[$departure])) return false;
+  if (calc_distance($lat, $lon, $airport[$departure]['lat'], $airport[$departure]['lon']) < 3 && $spd < 40) {
+    return true;
+  }
+
+  return false;
+}
+
+function arrivalEst($lat, $lon, $spd, $arrival, $status)
+{
+  global $readpdo, $prepareds, $airport;
+
+  if ($status != "En-Route") {
+    return 0;
+  }
+
+  if (!$arrival || $spd <= 0) {
+    return;
+  }
+
+  if (!isset($airport[$arrival])) {
+    $stmt = $readpdo->prepare($prepareds['select_airport'])->execute([":id" => $arrival]);
+    $row = $stmt->fetch();
+    if ($row) {
+      $airport[$arrival]['lat'] = $row['lat'];
+      $airport[$arrival]['lon'] = $row['lon'];
+      $airport[$arrival]['elevation'] = $row['elevation'];
+    } else {
+      return false;
+    }
+  }
+  $dist = calc_distance($lat, $lon, $airport[$arrival]['lat'], $airport[$arrival]['lon']);
+  $time = $dist / $spd; // Ground speed estimate
+  $hr = floor($time);
+  $min = intval((($hr - $time) + .25) * 60);
+  $est = new DateTime();
+  $est->add(new DateInterval("P" . $hr . "H" . $min . "M"));
+  return $est->format("Y-m-d H:i:s");
+}
+
+function airborne($spd)
+{
+  if ($spd > 50) {
+    return true;
+  }
+
+  return false;
+}
+
+function calc_distance($lat1, $lon1, $lat2, $lon2)
+{
+  $lat1 = deg2rad(floatval($lat1));
+  $lon1 = deg2rad(floatval($lon1));
+  $lat2 = deg2rad(floatval($lat2));
+  $lon2 = deg2rad(floatval($lon2));
+  $theta = $lon1 - $lon2;
+  $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+  $dist = acos($dist);
+  $dist = rad2deg($dist);
+  return abs($dist * 60 * 1.1515 * 0.8684);
+}
---
+++ parse.php
@@ -0,0 +1,196 @@
+<?php
+require(".config.php");
+
+require("global.php");
+
+if (file_exists(".lastts")) {
+  $lastts = file_get_contents(".lastts");
+}
+
+if (!file_exists(config('DATAFILE'))) {
+  die "No datafile defined or does not exist.";
+}
+
+// Make sure data file complete, otherwise wait a second and try again up to 10 times.
+exec("sed -n \"/;   END/p\" " . config('DATAFILE'), $output);
+$ready = false; $count = 0;
+if (isset($output[0]) && $output[0] == ";  END") {
+  $ready = true;
+} else {
+  $count++;
+  if ($count >= 10) { echo "Failed"; exit;}
+  sleep(1);
+}
+
+unset($output);
+// Get update timestamp, don't duplicate updates.
+exec("sed -n -r \"s/UPDATE = ([0-9]+)/\\1/p\" " . config('DATAFILE'), $output);
+if ($output[0] == $lastts) {
+  exit;                         // We got this TS already
+}
+
+$current_update = $output[0];
+
+$fp = fopen(".lastts", "w");
+fwrite($fp, $output[0]);
+fclose($fp);
+
+$stream = file(config("DATAFILE"), FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$in_clients = false;
+foreach ($stream as $line) {
+  if (preg_match("/^!CLIENTS:/", $line)) {
+    $in_clients = true;
+    continue;
+  }
+  elseif (preg_match("/^!/", $line) && $in_clients) { $in_clients = false; }
+
+  if (!$in_clients || preg_match("/^;/", $line)) continue;
+
+  process_line($line);
+
+  process_missing();
+}
+
+function process_line($line) {
+  global $pdo, $readpdo, $prepareds, $current_update;
+
+  $data = explode(":", $line);
+  // Skip bad data or ATC clients
+  if (!$data[cid]) continue;
+  if ($data[clienttype] == "ATC") continue;
+  if (!$data[planned_depairport] || !$data[planned_destairport]) continue;
+  if (!$data[latitude] || !$data[longitude]) continue;
+
+  // Little reformatting
+  $data[callsign] = str_replace("-", "", $data[callsign]);
+  $data[planned_altitude] = preg_replace("/[FAL]+/", "", $data[planned_altitude]); // Change ICAO and FL350 altitude entries into VATSIM-standard
+  if (strlen($data[planned_altitude]) < 4) $data[planned_altitude] = $data[planned_altitude] . "00"; // Make altitudes consistent
+  $data[planned_route] = str_replace("DCT", "", $data[planned_route]);
+  $data[planned_route] = preg_replace("/[\+\.]/", " ", $data[planned_route]); // Ignore VATSIM ATC amendment and misc. markings
+  $data[planned_route] = preg_replace("/\s+/", " ", trim($data[planned_route])); // Remove extra spaces
+
+  $new = 0;
+  $stmt = $pdo->prepare($prepareds['select_flight'])->execute([
+    ':callsign' => $data[callsign],
+    ':vatsim_id' => $data[cid]
+  ]);
+  $flight = $stmt->fetch();
+  if ($flight && $flight['status'] == "Incomplete" && $flight['departure'] == substr($data[planned_depairport, 0, 4])
+    && $flight['arrival'] == substr($data[planned_destairport], 0, 4) && !check_departure($data[latitude], $data[longitude], $data[groundspeed], $data[planned_destairport])) {
+      // Resuming flight
+      $flight['status'] = "En-Route";
+  } elseif (!$flight || ($flight['status'] == "Arrived" && !checkArrival($data[latitude], $data[longitude], $data[groundspeed], $data[planned_destairport]))) {
+    $new = 1;
+    $flight = [
+      'callsign' => $data[callsign],
+      'vatsim_id' => $data[cid]
+    ];
+  }
+
+  $flight['lat'] = $data[latitude];
+  $flight['lon'] = $data[longitude];
+  $flight['alt'] = $data[altitude];
+  $flight['hdg'] = $data[heading];
+  $flight['spd'] = $data[groundspeed];
+  $flight['route'] = $data[planned_route];
+  $flight['remarks'] = $data[planned_remarks];
+  // For aircraft type, filter out excess info
+  // Set aircraft, ensure to filter out things like (2H/) and (/L)
+  if ( preg_match("/^(?:.\/)?([^\/]+)(?:\/.)?/", $data[planned_aircraft], $matches)) {
+    $flight['aircraft_type'] = substr($matches[1], 0, 4);
+  } else {
+    $flight['aircraft_type'] = $data[planned_aircraft];
+  }
+  // Update flight plan details
+  $flight['departure'] = substr($data[planned_depairport], 0, 4);
+  $flight['arrival'] = substr($data[planned_destairport], 0, 4);
+  $flight['planned_alt'] = $data[planned_altitude];
+  $changedstatus = 0;
+
+  if (($flight['status'] == "En-Route" || $flight['status'] == "Incomplete") && checkArrival($flight['lat'], $flight['lon'], $flight['spd'], $flight['arrival'])) {
+    $flight['arrived_at'] = date("Y-m-d H:i:s");
+    $changedstatus = 1;
+    $flight['status'] = "Arrived";
+  } elseif ($flight['status'] == "Unknown" || $flight['status'] == "Incomplete") {
+    // Try to set a proper status
+    if (checkDeparture($flight['lat'], $flight['lon'], $flight['spd'], $flight['departure'])) {
+      $flight['status'] = "Departing Soon"; $changedstatus = 1;
+    } elseif (checkArrival($flight['lat'], $flight['lon'], $flight['spd'], $flight['arrival'])) {
+      $flight['status'] = "Arrived"; $changedstatus = 1;
+    } elseif (airborne($flight['spd'])) {
+      $flight['status'] = "En-Route"; $changedstatus = 1;
+    } else {
+      $flight->status = "Unknown"; $changedstatus = 1;    // Should never get here unless the plane diverted elsewhere without a FP change
+    }
+  } elseif (($flight['status'] == "Departing Soon" || $flight['status'] == "Incomplete") && airborne($flight['spd'])) {
+    $flight['departed_at'] = ($flight['departed_at']) ? $flight['departed_at'] : date("Y-m-d H:i:s"));
+    $flight['status'] = "En-Route";
+    $changedstatus = 1;
+  }
+
+  $flight['last_update'] = $current_update;
+  $flight['missing_count'] = 0;
+  if ($flight['status'] == "En-Route") {
+    $flight['arrival_est'] = arrivalEst($flight['lat'],$flight['lon'],$flight['spd'],$flight['arrival'],$flight['status']);
+  }
+
+  if (!$new) {
+    $allowed = ['id','aircraft_type','departure','arrival','planned_alt','route','remarks','status','departed_at','arrived_at','arrival_est','lat','lon','alt','hdg','spd','last_update','missing_count'];
+    $pdo->prepare($prepareds['update_flight'])->execute(
+      array_filter($flight, function($key) use ($allowed) { return in_array($key, $allowed);}, ARRAY_FILTER_USE_KEY)
+    );
+  } else {
+    $allowed = ['callsign','vatsim_id','aircraft_type','departure','arrival','planned_alt','route','remarks','status','lat','lon','alt','hdg','spd','last_update'];
+    $pdo->prepare($prepareds['insert_flight'])->execute(
+      array_filter($flight, function($key) use ($allowed) { return in_array($key, $allowed);}, ARRAY_FILTER_USE_KEY)
+    );
+    $flight['id'] = $pdo->lastInsertId();
+  }
+
+  $interval = 10;
+  if ($flight['spd'] >= 250) {
+    $interval = 2;
+  } elseif ($flight['spd'] >= 100) {
+    $interval = 5;
+  }
+
+  $interval = 0;
+  $row = $pdo->prepare($prepareds['select_position'])->execute(['flight_id' => $flight['id']]);
+  if ($row) {
+    $lastposition = new DateTime($row['created_at']);
+    $posinterval = $lastpos->diff(new DateTime("now"), true);
+    $posinterval = $lastpos->format("%i");
+  }
+  if (!$row || ($row && $posinterval >= $interval) || $changedstatus || $new) {
+    $pdo->prepare($prepareds['insert_position'])->execute([
+      'id'        => $flight['id'] . '-' . $current_update,
+      'flight_id' => $flight['id'],
+      'lat'       => $flight['lat'],
+      'lon'       => $flight['lon'],
+      'alt'       => $flight['alt'],
+      'spd'       => $flight['spd'],
+      'hdg'       => $flight['hdg']
+    ]);
+  }
+}
+
+function process_missing() {
+  global $readpdo, $pdo, $prepareds, $current_update;
+
+  $stmt = $readpdo->prepare($prepareds['select_missing'])->execute(['current_update' => $current_update]);
+  while ($row = $stmt->fetch()) {
+    $pdo->prepare($prepareds['update_missing_count'])->execute(['id' => $row['id'], 'missing_count' => $row['missing_count'] + 1]);
+
+    $row['missing_count'] += 1; // For processing's sake
+
+    if ($row['missing_count'] >= 5) {
+      if ($row['status'] == "Departing Soon") {
+        $pdo->prepare($prepareds['delete_flight'])->execute(['id' => $flight['id']]);
+        $pdo->prepare($prepareds['delete_positions'])->execute(['flight_id' => $flight['id']]);
+      } else {
+        $pdo->prepare($prepareds['update_missing_count'])->execute(['id' => $flight['id'], 'missing_count' => 0]);
+        $pdo->prepare($prepareds['update_status'])->execute(['id' => $flight['id'], 'status' => 'Incomplete']);
+      }
+    }
+  }
+}
