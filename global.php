f91be1fc75aeaf1b6c207170d7f8a30fd373fc81
Daniel Hawton <daniel@hawton.com>
2017-07-23 02:16:54 -0800

    fix math?

--- global.php
+++ global.php
@@ -174,12 +174,8 @@ function airborne($spd)
 
 function calc_distance($lat1, $lon1, $lat2, $lon2)
 {
-  $lat1 = deg2rad(floatval($lat1));
-  $lon1 = deg2rad(floatval($lon1));
-  $lat2 = deg2rad(floatval($lat2));
-  $lon2 = deg2rad(floatval($lon2));
   $theta = $lon1 - $lon2;
-  $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+  $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
   $dist = acos($dist);
   $dist = rad2deg($dist);
   return abs($dist * 60 * 1.1515 * 0.8684);
